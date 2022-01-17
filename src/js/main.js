8d4caf15f30b611ec76b39093def36bb25d0a357
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2022-01-17 17:28:06 +0300

    Реализация работы отправки данных на сервер

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 import modals from "./modules/modals.js";
 import sliders from "./modules/sliders.js";
-
+import forms from "./modules/forms.js";
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     modals();
     sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
 })
